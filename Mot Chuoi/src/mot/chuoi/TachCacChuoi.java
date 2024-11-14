5f414f6737864f552cc1ed7d574959df348f6c43
Nguyenlehuyhoang <154437878+Nguyenlehuyhoang@users.noreply.github.com>
2024-11-14 11:54:19 +0700

    Add files via upload

---
+++ Mot Chuoi/src/mot/chuoi/TachCacChuoi.java
@@ -0,0 +1,18 @@
+
+package mot.chuoi;
+
+public class TachCacChuoi {
+    public static String[] tachTuTrongChuoi(String chuoi) {
+        return chuoi.split(" ");
+    }
+
+    public static void main(String[] args) {
+        String chuoi = "Duy Tan university";
+        String[] tu = tachTuTrongChuoi(chuoi);
+
+        System.out.println("Cac tu trong chuoi:");
+        for (String t : tu) {
+            System.out.println(t);
+        }
+    }
+}
